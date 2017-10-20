93ed812ae8a14678a26c3916b0103d39219e5887
Curtis Maloney <curtis@tinbrain.net>
2017-10-20 21:54:34 +1100

    Fix m2m field logic

--- closure_tree/fields.py
+++ closure_tree/fields.py
@@ -1,5 +1,6 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.db.models.fields.related import resolve_relation
 from django.db.models.utils import make_model_tuple
 from django.utils.translation import gettext_lazy as _
 
@@ -9,52 +10,53 @@ class ClosureManyToManyField(models.ManyToManyField):
     Pre-configured M2M that defines a 'through' model automatically for the closure table.
     '''
     def __init__(self, *args, **kwargs):
-        super().__init__('self', related_name='ancestors', symmetrical=False, **kwargs)
+        super().__init__(to='self', related_name='ancestors', symmetrical=False, *args, **kwargs)
 
     def contribute_to_class(self, cls, name, **kwargs):
-        if cls._meta.abstract:
-            return
-        # Define through table
-        to_model = cls
-        name = '%s_Closure' % cls._meta.object_name
+        print(cls, name, **kwargs)
+        if not cls._meta.abstract:
+            # Define through table
+            to_model = resolve_relation(cls, self.remote_field.model)
 
-        to = make_model_tuple(to_model)[1]
-        from_ = cls._meta.model_name
+            name = '%s_Closure' % cls._meta.object_name
 
-        to = 'to_%s' % to
-        from_ = 'from_%s' % from_
+            to = make_model_tuple(to_model)[1]
+            from_ = cls._meta.model_name
 
-        meta = type('Meta', (), {
-            'db_table': '%s_closure' % cls._meta.db_table,
-            'auto_created': cls,
-            'app_label': cls._meta.app_label,
-            'db_tablespace': cls._meta.db_tablespace,
-            'unique_together': (from_, to),
-            'verbose_name': _('%(from)s-%(to)s relationship') % {'from': from_, 'to': to},
-            'verbose_name_plural': _('%(from)s-%(to)s relationships') % {'from': from_, 'to': to},
-            'apps': self.model._meta.apps,
-            'managed': False,
-        })
-        # Construct and set the new class.
-        self.remote_field.through = type(name, (models.Model,), {
-            'Meta': meta,
-            '__module__': cls.__module__,
-            'path': ArrayField(base_field=models.IntegerField(), primary_key=True),
-            'ancestor': models.ForeignKey(
-                cls,
-                related_name='%s+' % name,
-                db_tablespace=self.db_tablespace,
-                db_constraint=self.remote_field.db_constraint,
-                on_delete=CASCADE,
-            ),
-            'descendant': models.ForeignKey(
-                cls,
-                related_name='%s+' % name,
-                db_tablespace=self.db_tablespace,
-                db_constraint=self.remote_field.db_constraint,
-                on_delete=CASCADE,
-            ),
-            'depth': models.IntegerField(),
-        })
+            to = 'to_%s' % to
+            from_ = 'from_%s' % from_
+
+            meta = type('Meta', (), {
+                'db_table': '%s_closure' % cls._meta.db_table,
+                'auto_created': cls,
+                'app_label': cls._meta.app_label,
+                'db_tablespace': cls._meta.db_tablespace,
+                'unique_together': (from_, to),
+                'verbose_name': _('%(from)s-%(to)s relationship') % {'from': from_, 'to': to},
+                'verbose_name_plural': _('%(from)s-%(to)s relationships') % {'from': from_, 'to': to},
+                'apps': self.model._meta.apps,
+                'managed': False,
+            })
+            # Construct and set the new class.
+            self.remote_field.through = type(name, (models.Model,), {
+                'Meta': meta,
+                '__module__': cls.__module__,
+                'path': ArrayField(base_field=models.IntegerField(), primary_key=True),
+                'ancestor': models.ForeignKey(
+                    cls,
+                    related_name='%s+' % name,
+                    db_tablespace=self.db_tablespace,
+                    db_constraint=self.remote_field.db_constraint,
+                    on_delete=models.CASCADE,
+                ),
+                'descendant': models.ForeignKey(
+                    cls,
+                    related_name='%s+' % name,
+                    db_tablespace=self.db_tablespace,
+                    db_constraint=self.remote_field.db_constraint,
+                    on_delete=models.CASCADE,
+                ),
+                'depth': models.IntegerField(),
+            })
 
         super().contribute_to_class(cls, name, **kwargs)
--- closure_tree/models.py
+++ closure_tree/models.py
@@ -5,7 +5,7 @@ from .fields import ClosureManyToManyField
 
 class Node(models.Model):
     node_id = models.AutoField(primary_key=True)
-    parent = models.ForeignKey('self', related_name='children', null=True, blank=True)
+    parent = models.ForeignKey('self', related_name='children', null=True, blank=True, on_delete=models.CASCADE)
 
     descendants = ClosureManyToManyField()
 
