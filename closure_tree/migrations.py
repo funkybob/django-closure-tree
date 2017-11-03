d895aef25ff8d42ed7e142d48ef2d69c8a5148bf
Curtis Maloney <curtis@tinbrain.net>
2017-11-03 16:08:27 +1100

    Fix reverse migration

--- closure_tree/migrations.py
+++ closure_tree/migrations.py
@@ -2,6 +2,7 @@ from django.db.migrations.operations.special import RunSQL
 
 
 class CreateTreeClosure(RunSQL):
+    reversible = True
 
     def __init__(self, model_name):
         self.model_name = model_name.lower()
@@ -30,9 +31,10 @@ WHERE parent_id IS NOT NULL;
     'view': schema_editor.connection.ops.quote_name(view_name),
 })
 
+
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
         model = from_state.apps.get_model(app_label, self.model_name)
-        view_name = model_meta.db_table + '_closure'
+        view_name = model._meta.db_table + '_closure'
         schema_editor.execute('''DROP VIEW IF EXISTS %s;''' % (
             schema_editor.connection.ops.quote_name(view_name)
         ))
