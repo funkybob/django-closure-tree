cfb329a879f6df8a80eb20df06beb8bcc583132e
Matthew Schinckel <matt@schinckel.net>
2018-05-30 15:46:07 +0930

    Support _any_ table structure that has a single self-reference.

--- README.rst
+++ README.rst
@@ -7,7 +7,7 @@ Abstract base model for creating a Closure Tree using a recursive Postgres view.
 https://schinckel.net/2016/01/27/django-trees-via-closure-view/
 
 Usage
-=====
+-----
 
 Inherit from the Node model:
 
@@ -48,3 +48,23 @@ Add the CreateTreeClosure migration step:
            CreateTreeClosure('MyNode'),
        ]
 
+
+Usage in an existing app
+------------------------
+
+Add a field for the closure table reference to your model that already has a self-referencing
+foreign key.
+
+.. code-block:: python
+
+    from closure_tree.fields import ClosureManyToManyField
+
+    class MyModel(models.Model):
+        parent = models.ForeignKey('self', related_name='children', null=True, blank=True, on_delete=models.CASCADE)
+
+        ...
+
+        descendants = ClosureManyToManyField('self', symmetrical=False, related_name='ancestors')
+
+
+And create a migration (as above).
--- closure_tree/migrations.py
+++ closure_tree/migrations.py
@@ -10,26 +10,32 @@ class CreateTreeClosure(RunSQL):
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         model = from_state.apps.get_model(app_label, self.model_name)
-        view_name = model._meta.db_table + '_closure'
+        table_name = model._meta.db_table
+        view_name = table_name + '_closure'
+        node_id = model._meta.pk.column
+        parent_id = [field.column for field in model._meta.fields if field.rel and field.rel.to is model]
+        if len(parent_id) != 1:
+            raise Exception('Unable to detect self-referential field.')
 
         self.sql = '''
 CREATE OR REPLACE RECURSIVE VIEW {view}(path, ancestor_id, descendant_id, depth) AS
 
-SELECT ARRAY[node_id], node_id, node_id, 0
+SELECT ARRAY[{node_id}], {node_id}, {node_id}, 0
 FROM {table}
 
 UNION ALL
 
-SELECT parent_id || path, parent_id, descendant_id, depth + 1
+SELECT {table}.{parent_id} || {view}.path, {table}.{parent_id}, {view}.descendant_id, {view}.depth + 1
 FROM {table}
-INNER JOIN {view} ON (ancestor_id = node_id)
-WHERE parent_id IS NOT NULL;
+INNER JOIN {view} ON ({view}.ancestor_id = {table}.{node_id})
+WHERE {table}.{parent_id} IS NOT NULL;
 '''.format(
     table=schema_editor.connection.ops.quote_name(model._meta.db_table),
     view=schema_editor.connection.ops.quote_name(view_name),
+    node_id=schema_editor.connection.ops.quote_name(node_id),
+    parent_id=schema_editor.connection.ops.quote_name(parent_id),
 )
         super().database_forwards(app_label, schema_editor, from_state, to_state)
-
 
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
         model = from_state.apps.get_model(app_label, self.model_name)
